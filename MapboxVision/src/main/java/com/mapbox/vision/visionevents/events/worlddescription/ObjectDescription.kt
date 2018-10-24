4ea72501f379afbf2b02ce56d5a18e3ff441d83d
Dmitry Yunitsky <dmitry.yunitsky@gmail.com>
2018-10-24 16:49:09 +0300

    Update distance to car APIs.

--- MapboxVision/src/main/java/com/mapbox/vision/visionevents/events/worlddescription/ObjectDescription.kt
+++ MapboxVision/src/main/java/com/mapbox/vision/visionevents/events/worlddescription/ObjectDescription.kt
@@ -1,17 +1,17 @@
 package com.mapbox.vision.visionevents.events.worlddescription
 
 import com.mapbox.vision.visionevents.WorldCoordinate
-import com.mapbox.vision.visionevents.events.ObjectType
+import com.mapbox.vision.visionevents.events.detection.Detection
 
 /**
  * Represents detected object.
  *
  * @property distance from the car in meters
- * @property worldCoordinate coordinate, relative to us
- * @property objectType type of object
+ * @property worldCoordinate coordinate of object, relative to us
+ * @property detection of object
  */
 data class ObjectDescription(
         val distance: Double,
         val worldCoordinate: WorldCoordinate,
-        val objectType: ObjectType
+        val detection: Detection
 )
