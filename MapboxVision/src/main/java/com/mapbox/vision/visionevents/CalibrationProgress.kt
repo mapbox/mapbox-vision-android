97c78f72eb3edf42e61dea368c789c90877bc9bc
Dmitry Yunitsky <dmitry.yunitsky@gmail.com>
2018-11-15 16:01:47 +0300

    Fixed issue with calibration progress not aligned from [0,1] range to [0,100].
    (cherry picked from commit 57b8c75)

--- MapboxVision/src/main/java/com/mapbox/vision/visionevents/CalibrationProgress.kt
+++ MapboxVision/src/main/java/com/mapbox/vision/visionevents/CalibrationProgress.kt
@@ -2,10 +2,13 @@ package com.mapbox.vision.visionevents
 
 import com.mapbox.vision.core.buffers.CalibrationDataBuffer
 
+/**
+ * Calibration [progress] in percents [0, 100] is [completed].
+ */
 data class CalibrationProgress(val progress: Int, val completed: Boolean) {
     companion object {
         internal fun fromBuffer(calibrationDataBuffer: CalibrationDataBuffer) = CalibrationProgress(
-                progress = calibrationDataBuffer.calibrationProgress[0].toInt(),
+                progress = (calibrationDataBuffer.calibrationProgress[0] * 100).toInt(),
                 completed = calibrationDataBuffer.calibrationProgress[1] == 1f
         )
     }
